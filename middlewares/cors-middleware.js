fa1dac6b3725c4e06b69d935b8e71232e37dfbd6
Adolfo Onrubia <adolfo@onrubia.es>
2020-04-22 16:10:49 +0200

    setup remote db host

--- middlewares/cors-middleware.js
+++ middlewares/cors-middleware.js
@@ -1,26 +1,8 @@
-const cors = require('cors');
+const cors = require("cors");
 
 function initCors(app) {
-  const allowedOrigins = [
-    'http://localhost:3000',
-    'https://imputa-front.now.sh',
-    'https://imputa-front-git-dev.binomio.now.sh',
-  ];
-  app.use(
-    cors({
-      credentials: true,
-      origin(origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-          const msg =
-            'The CORS policy for this site does not allow access from the specified Origin.';
-          return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-      },
-    }),
-  );
-  console.log('INITIALIZE: cors middleware');
+  app.use(cors());
+  console.log("INITIALIZE: cors middleware");
 }
 
 module.exports = initCors;
